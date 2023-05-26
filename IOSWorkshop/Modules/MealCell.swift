559d4fc1755a6850c17da126844616efa955b6a6
saraeltlt <saraeltlt@hotmail.com>
2023-05-26 13:05:59 +0300

    add .gitignore file

---
+++ IOSWorkshop/Modules/MealCell.swift
@@ -0,0 +1,23 @@
+//
+//  MealCell.swift
+//  IOSWorkshop
+//
+//  Created by Sara Eltlt on 26/05/2023.
+//
+
+import UIKit
+
+class MealCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
