d24ba280fcdba7da046e78136c4366ff7783a9c7
saraeltlt <saraeltlt@hotmail.com>
2023-05-26 13:09:21 +0300

    This commit to add favCell

--- IOSWorkshop/Modules/MealCell.swift
+++ IOSWorkshop/Modules/MealCell.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class MealCell: UITableViewCell {
 
+    @IBOutlet weak var favBtn: UIButton!
+    @IBOutlet weak var imageBtnView: UIView!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var mealChefLabel: UILabel!
+    @IBOutlet weak var mealTypeLabel: UILabel!
+    @IBOutlet weak var mealServingLabel: UILabel!
+    @IBOutlet weak var bgImage: UIImageView!
+    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,4 +29,6 @@ class MealCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    @IBAction func addFavBtn(_ sender: UIButton) {
+    }
 }
