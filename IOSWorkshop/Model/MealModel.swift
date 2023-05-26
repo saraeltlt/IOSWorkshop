1bd2bdbb8daff86f4c7456dd41d313d5b42fc6ba
saraeltlt <saraeltlt@hotmail.com>
2023-05-26 17:38:27 +0300

    commit for hitting api

---
+++ IOSWorkshop/Model/MealModel.swift
@@ -0,0 +1,40 @@
+//
+//  MealModel.swift
+//  IOSWorkshop
+//
+//  Created by Sara Eltlt on 26/05/2023.
+//
+
+import Foundation
+class MealModel : Decodable {
+    var count: Int?
+    var results : [Meal]?
+    
+}
+
+class Meal : Decodable{
+    
+    var show_id: Int?
+    var name: String?
+    var  credits : [credit]? //chef and type
+    var thumbnail_url: String?
+    var video_url : String?
+    var num_servings : Int?
+    var instructions: [instruction]?
+    var tags: [ingredient]?
+    
+    
+}
+
+class credit : Decodable{
+    var name: String? //chef name
+    var type: String? //mealType
+    
+}
+class instruction : Decodable{
+    var display_text: String?
+}
+class ingredient : Decodable{
+    var display_name: String?
+    var type: String?
+}
